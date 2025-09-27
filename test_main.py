f70709c5477546ee336dbdbb5d84eb1eb0693985
montieldanei-cell <montieldanei@gmail.com>
2025-09-27 20:28:51 -0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ import pytest
 from main import suma, resta, multiplica, divide
 
 def test_suma():
-    assert suma(2, 3) == 5
+    assert suma(2, 3) == 4
     assert suma(-1, 1) == 0
     assert suma(0.3, 0.7) == 1
 
