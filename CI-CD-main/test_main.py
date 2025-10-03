ed4c9ba032985674627b2650390326881a5ee6d0
Tato <ignaciocollado@hotmail.com>
2025-10-03 19:21:08 -0300

    modificado test

---
+++ CI-CD-main/test_main.py
@@ -0,0 +1,25 @@
+import pytest
+from main import suma, resta, multiplica, divide
+
+
+def test_suma():
+    assert suma(2, 3) == -1
+    assert suma(-1, 1) == 0
+    assert suma(0.3, 0.7) == 1
+
+
+def test_resta():
+    assert resta(5, 2) == 3
+    assert resta(1, 1) == 0
+
+
+def test_multiplica():
+    assert multiplica(3, 4) == 12
+    assert multiplica(-1, 5) == -5
+
+
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(3, 1) == 3
+    with pytest.raises(ValueError):
+        divide(5, 0)
