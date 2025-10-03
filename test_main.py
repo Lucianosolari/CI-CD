e1146c0ac1be1587866ec56a9a6f5d879bbfc896
Carlo <cramirezdiaz.dev@gmail.com>
2025-10-03 19:14:38 -0300

    test1

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import suma, resta, multiplica, divide
 
 
 def test_suma():
-    assert suma(2, 3) == 2
+    assert suma(2, 3) == 5
     assert suma(-1, 1) == 0
     assert suma(0.3, 0.7) == 1
 
