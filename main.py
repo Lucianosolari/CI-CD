8cf2d01bd29b85c35e97cb3573d6e3e918f8dad0
Benjamin Burgos <benjaminburgost256@gmail.com>
2026-05-15 19:45:28 -0300

    feat: agrego funcion factorial

--- main.py
+++ main.py
@@ -22,3 +22,13 @@ def isString(value):
 
 def isPar(n):
     return n % 2 == 0
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("No se puede calcular el factorial de un número negativo")
+    if n == 0 or n == 1:
+        return 1
+    resultado = 1
+    for i in range(2, n + 1):
+        resultado *= i
+    return resultado
